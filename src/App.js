3cffab9c9bfdf1073cc5ccfa2e17ba3a21e083bc
Alex <alexander.s.carney.ug@dartmouth.edu>
2023-04-13 00:41:25 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useCallback } from 'react';
+import ReactFlow, { addEdge, applyNodeChanges, applyEdgeChanges, Background, Controls } from 'react-flow-renderer';
+import 'react-flow-renderer/dist/style.css';
+import { v4 as uuidv4 } from 'uuid';
+import CustomNode from './components/CustomNode';
+import TextUpdaterNode from "./components/TextUpdaterNode";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const initialElements = [];
+
+
+const Toolbar = ({ onAddNode, onAddEdge }) => (
+    <div
+        style={{
+            position: 'fixed',
+            top: '10px',
+            left: '10px',
+            zIndex: 1000,
+        }}
+    >
+        <button onClick={onAddNode}>Add Node</button>
+        <button onClick={onAddEdge}>Add Edge</button>
     </div>
-  );
-}
+);
+
+const nodeTypes = { textUpdater: TextUpdaterNode };
+
+const App = () => {
+    const [nodes, setNodes] = useState([]);
+    const [edges, setEdges] = useState([]);
+    const [selectedElement, setSelectedElement] = useState(null);
+
+    const onAddNode = () => {
+        const id = uuidv4();
+        const newNode = {
+            id,
+            type: 'textUpdater',
+            data: { label: `Node ${id}` },
+            position: { x: 250, y: 250 },
+        };
+        setNodes((prevNodes) => [...prevNodes, newNode]);
+    };
+
+    const onAddEdge = (connection) => {
+        const newEdge = {
+            id: uuidv4(),
+            source: connection.source,
+            target: connection.target,
+        };
+        setEdges((prevEdges) => [...prevEdges, newEdge]);
+    };
+
+    // const onNodesChange = useCallback(
+    //     (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    //     []
+    // );
+    //
+    // const onEdgesChange = useCallback(
+    //     (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    //     []
+    // );
+    //
+    // const onConnect = useCallback(
+    //     (params) =>
+    //         setEdges((eds) =>
+    //             addEdge(
+    //                 {
+    //                     id: uuidv4(),
+    //                     ...params,
+    //                 },
+    //                 eds
+    //             )
+    //         ),
+    //     []
+    // );
+    const onNodesChange = useCallback(
+        (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+        [setNodes]
+    );
+    const onEdgesChange = useCallback(
+        (changes) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+        [setEdges]
+    );
+    const onConnect = useCallback(
+        (connection) => setEdges((eds) => addEdge(connection, eds)),
+        [setEdges]
+    );
+
+
+    return (
+        <div style={{ height: '100vh' }}>
+            <Toolbar onAddNode={onAddNode} onAddEdge={onAddEdge} />
+            <div style={{ height: '90vh' }}>
+                <ReactFlow
+                    nodes={nodes}
+                    edges={edges}
+                    onConnect={onConnect}
+                    onNodesChange={onNodesChange}
+                    onEdgesChange={onEdgesChange}
+                    nodeTypes={nodeTypes}
+                    fitView
+
+                >
+                    <Background />
+                    <Controls />
+                </ReactFlow>
+
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/components/CustomNode.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Handle } from 'react-flow-renderer';
+
+const CustomNode = ({ id, data, selected }) => {
+    // ...
+};
+
+export default CustomNode;
